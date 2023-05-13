679c0095cc6396d1e3838c03195c56ce22ce0f85
Oleksandr <live.gp@gmail.com>
2023-05-13 14:32:30 +0300

    HW1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
 
+start.addEventListener('click', onStartClick);
+stop.addEventListener('click', onStopClick);
+
+function onStartClick() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    start.disabled = true;
+    stop.disabled = false;
+}
+
+function onStopClick() {
+    clearInterval(timerId);
+    start.disabled = false;
+    stop.disabled = true;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
