0da4556e51029bfac677a9411909d95ebfc0124b
Oleksandr <live.gp@gmail.com>
2023-05-13 19:59:50 +0300

    fix1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,18 +1,17 @@
 const start = document.querySelector('[data-start]');
 const stop = document.querySelector('[data-stop]');
+let timerId = null;
 
 start.addEventListener('click', onStartClick);
 stop.addEventListener('click', onStopClick);
 
 function onStartClick() {
-    clearInterval(timerId);
     timerId = setInterval(() => {
         document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
 
     start.disabled = true;
     stop.disabled = false;
-
 }
 
 function onStopClick() {
